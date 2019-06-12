0266db2314657192bb4fe9269cdf4f530a38c615
Arseny V. Povolotsky <arseny.povolotsky@epfl.ch>
2019-06-12 16:17:52 +0200

    Enable 'lint' check in CI (#4)

--- bluesnap/config.py
+++ bluesnap/config.py
@@ -1,30 +1,27 @@
+# Copyright (c) 2019, EPFL/Blue Brain Project
+
+# This file is part of BlueBrain SNAP library <https://github.com/BlueBrain/snap>
+
+# This library is free software; you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License version 3.0 as published
+# by the Free Software Foundation.
+
+# This library is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+
+# You should have received a copy of the GNU Lesser General Public License
+# along with this library; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 """
 SONATA network config parsing.
-"""
-
-"""
-Copyright (c) 2019, EPFL/Blue Brain Project
-
-This file is part of BlueBrain SNAP library <https://github.com/BlueBrain/snap>
-
-This library is free software; you can redistribute it and/or modify it under
-the terms of the GNU Lesser General Public License version 3.0 as published
-by the Free Software Foundation.
-
-This library is distributed in the hope that it will be useful, but WITHOUT
-ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
-details.
-
-You should have received a copy of the GNU Lesser General Public License
-along with this library; if not, write to the Free Software Foundation, Inc.,
-51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 """
 
 # TODO: move to `libsonata` library
 
 import collections
-import json
 import os.path
 
 import six
