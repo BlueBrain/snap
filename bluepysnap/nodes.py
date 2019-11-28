Fix names and add quaternion switch
* Quaternions are used sometimes but represented as 4 1D arrays

bluepysnap/nodes.py
@@ -321,14 +321,31 @@ class NodePopulation(object):
                 A 3x3 rotation matrix if a single node ID is passed as ``group``.
                 Otherwise a pandas Series with rotation matrices indexed by node IDs.
         """
-        # need to keep this rotation_angle ordering for euler2mat (expects z, y, x)
-        props = np.array(
-            ['rotation_angle_zaxis',
-             'rotation_angle_yaxis',
-             'rotation_angle_xaxis',
-             ]
-        )
+        # need to keep this quaternion ordering for quaternion2mat (expects w, x, y , z)
+        props = np.array([
+            "orientation_w",
+            "orientation_x",
+            "orientation_y",
+            "orientation_z"
+        ])
         props_mask = np.isin(props, list(self.property_names))
+        orientation_count = np.count_nonzero(props_mask)
+        if orientation_count == 4:
+            trans = utils.quaternion2mat
+        elif orientation_count in [1, 2, 3]:
+            raise BlueSnapError(
+                "Missing orientation fields. Should be 4 quaternions or euler angles or nothing")
+        else:
+            # need to keep this rotation_angle ordering for euler2mat (expects z, y, x)
+            props = np.array(
+                ['rotation_angle_zaxis',
+                 'rotation_angle_yaxis',
+                 'rotation_angle_xaxis',
+                 ]
+            )
+            props_mask = np.isin(props, list(self.property_names))
+            trans = utils.euler2mat
+
         result = self.get(group=group, properties=props[props_mask])
 
         def _get_values(prop):
@@ -339,8 +356,8 @@ class NodePopulation(object):
 
         args = [_get_values(prop) for prop in props]
         if isinstance(group, six.integer_types + (np.integer,)):
-            return utils.euler2mat(*args)[0]
-        return pd.Series(utils.euler2mat(*args), index=result.index, name='orientation')
+            return trans(*args)[0]
+        return pd.Series(trans(*args), index=result.index, name='orientation')
 
     def count(self, group=None):
         """Total number of nodes for a given node group.

bluepysnap/utils.py
@@ -74,11 +74,14 @@ def euler2mat(az, ay, ax):
     return [mm[..., i] for i in range(len(az))]
 
 
-def quaternion2mat(aq):
+def quaternion2mat(aqw, aqx, aqy, aqz):
     """Build 3x3 rotation matrices from quaternions.
 
     Args:
-        aq: array of quaternions (Nx4 NumPy array; float)
+        aqw: w component of quaternions (Nx1 NumPy array; float)
+        aqx: x component of quaternions (Nx1 NumPy array; float)
+        aqy: y component of quaternions (Nx1 NumPy array; float)
+        aqz: z component of quaternions (Nx1 NumPy array; float)
 
     Returns:
         List with Nx3x3 rotation matrices corresponding to each of N quaternions.
@@ -97,7 +100,7 @@ def quaternion2mat(aq):
         """
         return qs / np.sqrt(np.einsum('...i,...i', qs, qs)).reshape(-1, 1)
 
-    aq = np.asarray(aq, dtype=np.float64)
+    aq = np.dstack([np.asarray(aqw), np.asarray(aqx), np.asarray(aqy), np.asarray(aqz)])[0]
     aq = normalize_quaternions(aq)
 
     w = aq[:, 0]

tests/test_utils.py
@@ -58,7 +58,7 @@ def test_euler2mat():
 
 
 def test_quaternion2mat():
-    actual = test_module.quaternion2mat([[1, 1, 0, 0], [1, 0, 1, 0], [1, 0, 0, 1]])
+    actual = test_module.quaternion2mat([1, 1, 1], [1, 0, 0, ], [0, 1, 0], [0, 0, 1])
     expected = np.array([
         [
             [1., 0., 0.],