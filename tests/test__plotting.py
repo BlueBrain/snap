37e92f6a06f5d079e38b46336798ce9965829ead
tomdele <thomas.delemontex@epfl.ch>
2020-11-26 14:26:41 +0100

    Removed the specific for the plotting tests

--- tests/test__plotting.py
+++ tests/test__plotting.py
@@ -2,7 +2,6 @@ import sys
 
 import mock
 import pytest
-import six
 
 import bluepysnap._plotting as test_module
 
@@ -11,14 +10,6 @@ def test__get_pyplot():
     with mock.patch.dict(sys.modules, {'matplotlib.pyplot': None}):
         with pytest.raises(ImportError):
             test_module._get_pyplot()
-
-    if sys.platform == 'darwin' and six.PY2:
-        # Set a backend different to MacOSX (default on Mac OS) to avoid the error
-        #   RuntimeError: Python is not installed as a framework
-        # using Python 2.7 in a virtualenv on Mac OS.
-        # See also https://matplotlib.org/faq/osx_framework.html.
-        import matplotlib
-        matplotlib.use('pdf')
 
     import matplotlib.pyplot
     plt_test = test_module._get_pyplot()
