a7645ad0d2d6ac36db147b542e2328856ce5a8b6
Mike Gevaert <michael.gevaert@epfl.ch>
2022-04-05 16:06:07 +0200

    fix format

--- setup.py
+++ setup.py
@@ -45,8 +45,8 @@ setup(
     install_requires=[
         "cached_property>=1.0",
         "h5py>=3.0.1,<4.0.0",
-        "libsonata>=0.1.13,<1.0.0",
         'jsonschema==4.4.0',  # tested with this version
+        "libsonata>=0.1.13,<1.0.0",
         "morphio>=3.0.0,<4.0.0",
         "morph-tool>=2.4.3,<3.0.0",
         "numpy>=1.8,<2.0.0",
