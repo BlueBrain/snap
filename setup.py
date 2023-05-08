6c0260d3253d37b1e6aeb454a414c16b31101883
eduard.subert <eduard.subert@epfl.ch>
2023-05-08 10:22:43 +0200

    upgrade libsonata

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
         "cached_property>=1.0",
         "h5py>=3.0.1,<4.0.0",
         "jsonschema>=4.0.0,<5.0.0",
-        "libsonata>=0.1.17,<1.0.0",
+        "libsonata>=0.1.20,<1.0.0",
         "morphio>=3.0.0,<4.0.0",
         "morph-tool>=2.4.3,<3.0.0",
         "numpy>=1.8",
